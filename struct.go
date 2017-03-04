c1e353a03ba4e5ecf91d6c4ec042d83fda781fb4
Yuki Fukuyama <yfukuy0118@gmail.com>
2017-03-04 19:41:19 +0900

    Olderをexportしないように関数名変更

--- struct.go
+++ struct.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-// Person exported struct
-type Person struct {
+// person exported struct
+type person struct {
 	name string
 	age  int
 }
 
 // Older return older person and diff
-func Older(p1 Person, p2 Person) (Person, int) {
+func older(p1 person, p2 person) (person, int) {
 	if p1.age > p2.age {
 		return p1, p1.age - p2.age
 	}
@@ -17,19 +17,19 @@ func Older(p1 Person, p2 Person) (Person, int) {
 }
 
 func main() {
-	var tom Person
+	var tom person
 	tom.name, tom.age = "tom", 20
 
-	bob := Person{age: 14, name: "bob"}
-	paul := Person{"paul", 23}
+	bob := person{age: 14, name: "bob"}
+	paul := person{"paul", 23}
 
-	roundOneWinner, diff := Older(tom, bob)
+	roundOneWinner, diff := older(tom, bob)
 	fmt.Printf("%s vs %s. winner is %s. diff is %d.\n", tom.name, bob.name, roundOneWinner.name, diff)
 
-	roundTwoWinner, diff := Older(bob, paul)
+	roundTwoWinner, diff := older(bob, paul)
 	fmt.Printf("%s vs %s. winner is %s. diff is %d.\n", bob.name, paul.name, roundTwoWinner.name, diff)
 
-	roundThreeWinner, diff := Older(paul, tom)
+	roundThreeWinner, diff := older(paul, tom)
 	fmt.Printf("%s vs %s. winner is %s. diff is %d\n", paul.name, tom.name, roundThreeWinner.name, diff)
 
 }
